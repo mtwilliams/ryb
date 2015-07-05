93e6ff98673f70f8d0db97d97825bc5930462fcd
Michael Williams <m.t.williams@live.com>
2015-07-04 20:40:41 -0700

    Actually, use `0.0.0.dev` while working on pre-release.

--- lib/ryb/version.rb
+++ lib/ryb/version.rb
@@ -1,6 +1,6 @@
 module Ryb
   module VERSION #:nodoc:
-    MAJOR, MINOR, PATCH, PRE = [0, 1, 0, 'dev']
+    MAJOR, MINOR, PATCH, PRE = [0, 0, 0, 'dev']
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
   end
 
