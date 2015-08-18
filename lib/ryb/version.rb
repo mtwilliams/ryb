93d549d8e3a879cc9941ca2d5f1d635c1761a9f1
Michael Williams <m.t.williams@live.com>
2015-08-17 18:49:49 -0700

    Bump version.

--- lib/ryb/version.rb
+++ lib/ryb/version.rb
@@ -1,6 +1,6 @@
 module Ryb
   module VERSION #:nodoc:
-    MAJOR, MINOR, PATCH, PRE = [0, 1, 2, 1]
+    MAJOR, MINOR, PATCH, PRE = [0, 1, 3, 1]
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
   end
 
